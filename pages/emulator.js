a2117ffcfad72717ac9f9cde9309ecd3737318ab
Claudio <71095411+itsthered1@users.noreply.github.com>
2022-10-07 20:07:53 +0000

    - Testing with global.css - Added emulator.js

--- pages/emulator.js
+++ pages/emulator.js
@@ -2,16 +2,17 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 
-export default function Home() {
+export default function Emulator() {
   return (
-    <>
+   <>
     <header>
       <h1 class="typewriter"><a href="/">claudio rojas</a></h1>
     </header>
 
-    <body class="center, paragraph">
-      <p1>Under Construction...</p1>
+    <body class="center">
+      <p1>Upload a ROM...</p1>
     </body>
-    </>
-  );
+     
+   </> 
+  )
 }
